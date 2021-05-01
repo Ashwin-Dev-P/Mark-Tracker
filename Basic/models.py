6b3d041adf133c984b6e1873596029f5558cf2d2
Ashwin Dev <ashwindev1462001@gmail.com>
2021-05-01 15:52:53 +0530

    Primary key added manually in models to avoid warnings

--- Basic/models.py
+++ Basic/models.py
@@ -4,11 +4,14 @@ from django.core.validators import MaxValueValidator
 
 # Create your models here.
 class Departments(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=100)
 class semester(models.Model):
+    id = models.BigAutoField(primary_key=True)
     semester = models.PositiveSmallIntegerField( validators=[MaxValueValidator(12)])
 
 class markDetails(models.Model):
+    id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(User,default=None,on_delete=models.CASCADE,)
     semester = models.PositiveSmallIntegerField( validators=[MaxValueValidator(12)])
     subject_name = models.CharField(max_length=100)
@@ -17,10 +20,12 @@ class markDetails(models.Model):
 
 
 class additionalInfo(models.Model):
+    id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(User,default=None,on_delete=models.CASCADE,)
     Department = models.ForeignKey(Departments,default=None,on_delete=models.CASCADE)
     current_semester = models.ForeignKey(semester,default=None,validators=[MaxValueValidator(101)],on_delete=models.CASCADE)
     privacy = models.PositiveSmallIntegerField(default=0,validators = [MaxValueValidator(1)]);
 
 class subjects(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=100)
